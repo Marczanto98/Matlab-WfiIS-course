a9a86ca2507da4e047c59e7d3d37f82532745a3b
Marek <mark-now@wp.pl>
2020-05-21 23:51:31 +0200

    lab1 done

---
+++ lab1/przetworz.m
@@ -0,0 +1,22 @@
+function przetworz(v, przel)
+maks = max(v); 
+minim = min(v);
+parz = numel(find(mod(v,2)==0)); 
+niep = numel(find(mod(v,2)~=0));
+niezer = numel(find(v));
+modified_v = v(1:end-1);
+if przel == 1
+    moj_sin = sin(modified_v);
+elseif przel == 2
+    moj_sin = cos(modified_v);
+else
+    disp("BLAD")
+    return
+end
+
+maks
+minim
+parz
+niep
+niezer
+moj_sin
